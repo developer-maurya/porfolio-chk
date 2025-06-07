053d2b3d1797b38a8796170b1f2056336bea720c
Chitranjan kumar <chitranjan75075@gmail.com>
2025-06-07 23:59:39 +0530

    first commit

--- index.js
+++ index.js
@@ -11,4 +11,25 @@ function opentab(tabname){
     document.getElementById(tabname).classList.add("active-tab")
 }
 
+var typed = new Typed('#element', {
+  strings: ['Frontend Developer', 'Web Developer', 'Graphic Designer', 'Web Designer'],
+  typeSpeed: 50,
+  backSpeed: 30,      
+  backDelay: 1000,    
+  loop: true          
+});
 
+// ---------------------------card--------------------------------    
+var tablinks = document.getElementsByClassName("work-item")
+var tabcards = document.getElementsByClassName("all-card")
+
+function opentab1(work){
+    for(let tablink of tablinks){
+        tablink.classList.remove("active-link1")
+    }
+     for(let tabcard of tabcards){
+        tabcard.classList.remove("active-tab1")
+    }
+    event.currentTarget.classList.add("active-link1")
+    document.getElementById(work).classList.add("active-tab1")
+}
